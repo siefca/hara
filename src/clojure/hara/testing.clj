d5f232d2e11b6526c574317946a7288d3cd07db6
zcaudate <z@caudate.me>
2012-11-19 16:42:44 +1100

    Added functions for testing

---
+++ src/clojure/hara/testing.clj
@@ -0,0 +1,19 @@
+(ns hara.testing)
+
+(defn is-atom [& [value]]
+  (fn [at]
+    (if (and (instance? clojure.lang.Atom at)
+             (= @at value))
+      true)))
+
+(defn is-ref [& [value]]
+  (fn [rf]
+    (if (and (instance? clojure.lang.Ref rf)
+             (= @rf value))
+      true)))
+
+(defn is-ova [& values]
+  (fn [ov]
+    (if (and (instance? hara.data.Ova ov)
+             (= (seq (persistent! ov)) values))
+      true)))
