e4b4035b4d749b3d07603cbba1ab4e8490573aa5
Chris Zheng <z@caudate.me>
2013-08-14 23:26:58 +1000

    Cleared Readme

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject im.chit/hara "1.0.1"
-  :description "Utilities library of Functions"
+  :description "General purpose utilities library "
   :url "http://github.com/zcaudate/hara"
   :license {:name "The MIT License"
             :url "http://http://opensource.org/licenses/MIT"}
