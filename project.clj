3c7eeb1f05bd6c0d39870f8c481bf29c91a07217
Chris Zheng <z@caudate.me>
2013-08-14 10:51:04 +1000

    Starting on 0.9.1

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject im.chit/hara "0.8.0-SNAPSHOT"
-  :description "Utilities library of Functions and Exploratory Programming"
+(defproject im.chit/hara "0.9.1"
+  :description "Utilities library of Functions"
   :url "http://github.com/zcaudate/hara"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "The MIT License"
+            :url "http://http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
   :profiles {:dev {:dependencies [[midje "1.5.1"]
                                   [clj-time "0.5.1"]]}})
