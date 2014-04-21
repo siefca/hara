5b253dc6946866bd5e8b4dfa61eb8fc84f6f58de
Chris Zheng <chris.zheng@auspost.com.au>
2014-04-21 10:26:48 +1000

    Removing core_tests.clj from testing

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject im.chit/hara "2.1.0"
+(defproject im.chit/hara "2.1.1-SNAPSHOT"
   :description "General purpose utilities library "
   :url "http://github.com/zcaudate/hara"
   :license {:name "The MIT License"
@@ -7,5 +7,5 @@
   :profiles {:dev {:dependencies [[midje "1.6.3"]]
                    :plugins [[lein-midje "3.1.3"]
                              [lein-midje-doc "0.0.21"]
-                             [lein-repack "0.1.2"]]}}
+                             [lein-repack "0.1.3"]]}}
   :repack {:levels 2})
