a3218ed062d083e54b713689b4720fbe96e452a8
Chris Zheng <chris.zheng@auspost.com.au>
2014-08-18 16:20:28 +1000

    Added readme stuff

--- README.md
+++ README.md
@@ -1,14 +1,28 @@
 # hara 
 [![Build Status](https://travis-ci.org/zcaudate/hara.png?branch=master)](https://travis-ci.org/zcaudate/hara)
 
-General purpose utilities library
+General purpose utilities library. Please see [finding a middle ground](http://z.caudate.me/finding-a-middle-ground/) for motivations and reasoning.
 
+## Installation
 
+Add to project.clj dependencies:
 
-## TODOS:
+```clojure
+[im.chit/hara "2.1.1"]
 
+or
+
+[im.chit/hara.<PACKAGE> "2.1.1"]
+
+or
+
+[im.chit/hara.<PACKAGE>.<NAMESPACE> "2.1.1"]
+```
+
+Where `PACKAGE` and `NAMESPACE` can be seen from the [API Documentation](http://docs.caudate.me/hara/). Please see documentation for examples of usage.
 
 ## License
-Copyright © 2013 Chris Zheng
+
+Copyright © 2014 Chris Zheng
 
 Distributed under the MIT License
--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
 (defproject im.chit/hara "2.1.1"
   :description "General purpose utilities library "
-  :url "http://github.com/zcaudate/hara"
+  :url "https://github.com/zcaudate/hara"
   :license {:name "The MIT License"
             :url "http://http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
   :profiles {:dev {:dependencies [[midje "1.6.3"]]
                    :plugins [[lein-midje "3.1.3"]
-                             [lein-midje-doc "0.0.22"]
-                             [lein-repack "0.1.3"]]}}
+                             [lein-midje-doc "0.0.23"]
+                             [lein-repack "0.1.4"]]}}
   :documentation {:files {"docs/index"
                         {:input "test/midje_doc/hara/outline.clj"
                          :title "hara"
